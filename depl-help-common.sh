64225582992ba239e38a0900acb965d69f518835
LFDM <1986gh@gmail.com>
2014-11-02 16:19:27 +0100

    Add bold output

--- depl-help-common.sh
+++ depl-help-common.sh
@@ -13,7 +13,11 @@ fi
 link_name () { echo "$app-$1-$2"; }
 link_path () { echo "$path/$(link_name $1 $2)"; }
 
+bold () {
+  echo "\033[1m$1\033[0m"
+}
+
 create_link () {
   ln -s $1 $2
-  echo "Linking $2 to $1"
+  echo -e "Linking $(bold $2) to $(bold $1)"
 }
