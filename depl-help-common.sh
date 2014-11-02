3450b311c1845ee5f5126e71a6a4b5a81c65819d
LFDM <1986gh@gmail.com>
2014-11-02 14:52:52 +0100

    Add some error handling

--- depl-help-common.sh
+++ depl-help-common.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+set -e
+
 app=$1
 total_number=5
 
-echo $app
+if [ -z $app ]; then
+  echo "Need an app name to run!"
+  exit 1
+fi
 
 link_name () { echo "$app-$1-$2"; }
 link_path () { echo "$path/$(link_name $1 $2)"; }
